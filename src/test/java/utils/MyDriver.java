74b2f2a9831fb6de17dab27874e2e467ca91bb9b
tduqueg <tduqueg@eafit.edu.co>
2024-02-22 12:03:27 -0500

    BasePage y BaseTest

---
+++ src/test/java/utils/MyDriver.java
@@ -0,0 +1,19 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyDriver {
+    private WebDriver driver;
+
+    public MyDriver(){
+        String path = System.getProperty("user.dir");
+        System.out.println(path);
+        System.setProperty("webdriver.chrome.driver", path + "\\drivers\\chromedriver.exe");
+        driver = new ChromeDriver();
+    }
+
+    public WebDriver getDriver(){
+        return this.driver;
+    }
+}
--- src/test/java/utils/baseTest/BaseTest.java
+++ src/test/java/utils/baseTest/BaseTest.java
@@ -1,4 +1,30 @@
 package utils.baseTest;
 
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+import utils.MyDriver;
+
 public class BaseTest {
+
+    MyDriver driver;
+
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters({"url"})
+    public void beforeMethod(String url){
+        driver = new MyDriver();
+        driver.getDriver().manage().window().maximize();
+        navigateTo(url);
+    }
+
+    public void navigateTo(String url){
+        driver.getDriver().get(url);
+    }
+
+
+    @AfterMethod()
+    public void afterMethod(){
+        driver.getDriver().close();
+    }
 }
